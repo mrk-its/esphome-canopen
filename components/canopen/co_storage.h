143a4e1742c223fa45b3ca203e449e041e227bd4
Mariusz Kryński <mrk@sed.pl>
2023-05-23 20:56:25 +0200

    add missing file

---
+++ components/canopen/co_storage.h
@@ -0,0 +1,11 @@
+#ifndef CO_STORE_H
+#define CO_STORE_H
+#include "co_core.h"
+
+extern CO_OBJ_TYPE StoreCommParams;
+#define CO_TSTORE  ((CO_OBJ_TYPE*)&StoreCommParams)
+
+extern CO_OBJ_TYPE ResetCommParams;
+#define CO_TRESET  ((CO_OBJ_TYPE*)&ResetCommParams)
+
+#endif
