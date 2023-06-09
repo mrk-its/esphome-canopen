a27668303e53ccb07efc6afe7072292c7ad88fcd
Mariusz Kryński <mrk@sed.pl>
2023-06-09 23:45:56 +0200

    add CO_TCMD16

--- components/canopen/co_cmd.cpp
+++ components/canopen/co_cmd.cpp
@@ -40,6 +40,44 @@ CO_OBJ_TYPE Cmd8 = {
     0,
 };
 
+uint32_t Cmd16Size (CO_OBJ *obj, CO_NODE *node, uint32_t width) {
+    const CO_OBJ_TYPE *uint32 = CO_TUNSIGNED16;
+    return uint32->Size(obj, node, width);
+}
+
+CO_ERR   Cmd16Init (CO_OBJ *obj, CO_NODE *node) {
+    return CO_ERR_NONE;
+}
+
+CO_ERR Cmd16Read(struct CO_OBJ_T *obj, struct CO_NODE_T *node, void *buffer, uint32_t size) {
+    ESP_LOGV(TAG, "Cmd32Read: key: %x", obj->Key);
+    const CO_OBJ_TYPE *uint32 = CO_TUNSIGNED16;
+    return uint32->Read(obj, node, buffer, size);
+}
+
+CO_ERR Cmd16Write(CO_OBJ *obj, CO_NODE *node, void *buffer, uint32_t size) {
+    ESP_LOGV(TAG, "Cmd32Write: key: %08x, val: %08x, size: %x", obj->Key, *((uint8_t *)buffer), size);
+
+    const CO_OBJ_TYPE *uint32 = CO_TUNSIGNED16;
+    CO_ERR result = uint32->Write(obj, node, buffer, size);
+    uint32_t index = obj->Key & 0xffffff00;
+
+    auto cmd_handlers = global_canopen->can_cmd_handlers;
+    auto it = cmd_handlers.find(index);
+    if(it != cmd_handlers.end()) {
+        it->second(buffer, size);
+    }
+    return result;
+}
+
+CO_OBJ_TYPE Cmd16 = {
+    Cmd16Size,
+    Cmd16Init,
+    Cmd16Read,
+    Cmd16Write,
+    0,
+};
+
 uint32_t Cmd32Size (CO_OBJ *obj, CO_NODE *node, uint32_t width) {
     const CO_OBJ_TYPE *uint32 = CO_TUNSIGNED32;
     return uint32->Size(obj, node, width);
--- components/canopen/co_cmd.h
+++ components/canopen/co_cmd.h
@@ -5,6 +5,9 @@
 extern CO_OBJ_TYPE Cmd8;
 #define CO_TCMD8  ((CO_OBJ_TYPE*)&Cmd8)
 
+extern CO_OBJ_TYPE Cmd16;
+#define CO_TCMD16  ((CO_OBJ_TYPE*)&Cmd16)
+
 extern CO_OBJ_TYPE Cmd32;
 #define CO_TCMD32  ((CO_OBJ_TYPE*)&Cmd32)
 
