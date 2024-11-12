97ee402186ca00e3b3225efa5f7c4e1ab324581f
Silvain <silvain.louis@iadys.com>
2024-11-12 17:03:43 +0100

    Change spec

--- components/canopen/node_spec.cpp
+++ components/canopen/node_spec.cpp
@@ -28,7 +28,10 @@
 #define APP_BAUDRATE 250000u       /* CAN baudrate                */
 #define APP_TMR_N 16u              /* Number of software timers   */
 #define APP_TICKS_PER_SEC 1000000u /* Timer clock frequency in Hz */
+
+#ifndef APP_OBJ_N
 #define APP_OBJ_N 512u             /* Object dictionary max size  */
+#endif
 
 /******************************************************************************
  * PRIVATE VARIABLES
