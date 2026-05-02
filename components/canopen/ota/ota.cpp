e24fb9b594915a60f7620a2dbd853c71c8acacd0
Mariusz Kryński <mrk@sed.pl>
2026-05-02 12:44:56 +0200

    OTA refactor
    - migrate to new esphome
    - use state machine instead of ota_finished_trigger automation

--- components/canopen/ota/ota.cpp
+++ components/canopen/ota/ota.cpp
@@ -1,8 +1,7 @@
 #include "ota.h"
 // #ifdef USE_OTA
 #include "esphome/components/md5/md5.h"
-#include "esphome/components/ota/ota_backend.h"
-#include "esphome/components/ota/ota_backend_esp_idf.h"
+#include "esphome/components/ota/ota_backend_factory.h"
 #include "esphome/core/application.h"
 #include "esphome/core/hal.h"
 #include "esphome/core/log.h"
@@ -20,52 +19,59 @@ void CanopenOTAComponent::setup() {
   ota::register_ota_platform(this);
 #endif
   backend = esphome::ota::make_ota_backend();
-
-  ota_finished_trigger = new canopen::OtaFinishedTrigger();
-  auto automation_id = new Automation<>(ota_finished_trigger);
-  auto delayaction_id = new DelayAction<>();
-  auto delayaction2_id = new DelayAction<>();
-
-  delayaction_id->set_component_source(LOG_STR("canopen.ota"));
-  App.register_component(delayaction_id);
-  delayaction_id->set_delay(1000);
-
-  delayaction2_id->set_component_source(LOG_STR("canopen.ota"));
-  App.register_component(delayaction2_id);
-  delayaction2_id->set_delay(1000);
-
-  auto ota_end_id = new LambdaAction<>([=]() -> void {
-    esphome::ota::OTAResponseTypes ret = esphome::ota::OTAResponseTypes::OTA_RESPONSE_OK;
-    if(!dry_run) {
-      ret = backend->end();
-    }
-    if(ret == esphome::ota::OTAResponseTypes::OTA_RESPONSE_OK) {
-      ESP_LOGI(TAG, "ota finished successfully, rebooting");
-    } else {
-      ESP_LOGE(TAG, "ota error: %d", ret);
-    }
-  });
-
-  auto reboot_action_id = new LambdaAction<>([=]() -> void {
-    if(!disable_ota_reboot) {
-      ESP_LOGI(TAG, "ota finished, rebooting");
-      App.safe_reboot();
-    }
-  });
-
-  automation_id->add_actions({delayaction_id, ota_end_id, delayaction2_id, reboot_action_id});
+  ota_state = OTA_STATE_IDLE;
 #ifdef OTA_COMPRESSION
   this->stream = z_stream{};
 #endif
 }
 
-void CanopenOTAComponent::loop() {}
+void CanopenOTAComponent::ota_finish() {
+  start_time = millis();
+  ota_state = OTA_STATE_WAIT1;
+}
+
+void CanopenOTAComponent::loop() {
+  switch (ota_state) {
+    case OTA_STATE_IDLE:
+      break;
+    case OTA_STATE_WAIT1: {
+      if (millis() - start_time < 1000) {
+        break;
+      }
+      // wait, then:
+      esphome::ota::OTAResponseTypes ret = esphome::ota::OTAResponseTypes::OTA_RESPONSE_OK;
+      if (!dry_run) {
+        ret = backend->end();
+      }
+      if (ret == esphome::ota::OTAResponseTypes::OTA_RESPONSE_OK) {
+        ESP_LOGI(TAG, "ota finished successfully, rebooting");
+      } else {
+        ESP_LOGE(TAG, "ota error: %d", ret);
+      }
+      start_time = millis();
+      ota_state = OTA_STATE_WAIT2;
+      break;
+    }
+    case OTA_STATE_WAIT2: {
+      if (millis() - start_time < 1000) {
+        break;
+      }
+      ota_state = OTA_STATE_IDLE;
+      // wait, then rebbot
+      if (!disable_ota_reboot) {
+        ESP_LOGI(TAG, "ota finished, rebooting");
+        App.safe_reboot();
+      }
+      break;
+    }
+  }
+}
 
 float CanopenOTAComponent::get_setup_priority() const { return setup_priority::LATE; }
 
 esphome::ota::OTAResponseTypes CanopenOTAComponent::begin(uint32_t size) {
 #ifdef OTA_COMPRESSION
-  if(this->stream.state) {
+  if (this->stream.state) {
     mz_deflateEnd(&this->stream);
   }
 
@@ -90,7 +96,7 @@ int CanopenOTAComponent::decompress() {
   if ((status == Z_STREAM_END) || (status == Z_OK && !this->stream.avail_out)) {
     uint32_t n = BUF_SIZE - this->stream.avail_out;
     this->written += n;
-    if(n > 0) {
+    if (n > 0) {
       ESP_LOGI(TAG, "writing %ld bytes to flash, total: %ld", n, this->written);
       auto ret = !dry_run ? backend->write(this->s_outbuf, n) : esphome::ota::OTAResponseTypes::OTA_RESPONSE_OK;
       if (ret != esphome::ota::OTAResponseTypes::OTA_RESPONSE_OK) {
@@ -107,13 +113,8 @@ int CanopenOTAComponent::decompress() {
 
 esphome::ota::OTAResponseTypes CanopenOTAComponent::write(uint8_t *data, size_t len) {
 #ifdef OTA_COMPRESSION
-  ESP_LOGV(
-    TAG,
-    "offset: %ld, len: %d, data: %02x %02x %02x %02x %02x %02x %02x",
-    this->received, len,
-    data[0], data[1], data[2], data[3],
-    data[3], data[4], data[6]
-  );
+  ESP_LOGV(TAG, "offset: %ld, len: %d, data: %02x %02x %02x %02x %02x %02x %02x", this->received, len, data[0], data[1],
+           data[2], data[3], data[3], data[4], data[6]);
   this->stream.next_in = data;
   this->stream.avail_in = len;
   this->received += len;
@@ -130,7 +131,7 @@ esphome::ota::OTAResponseTypes CanopenOTAComponent::write(uint8_t *data, size_t 
   }
 #else
   ESP_LOGI(TAG, "ota write %d bytes", len);
-  if(!dry_run) {
+  if (!dry_run) {
     return backend->write(data, len);
   }
 #endif
@@ -147,7 +148,7 @@ esphome::ota::OTAResponseTypes CanopenOTAComponent::end(const char *expected_md5
       if (!dry_run) {
         backend->set_update_md5(expected_md5);
       }
-      ota_finished_trigger->trigger();
+      this->ota_finish();
       // if (!ret) {
       //   ESP_LOGI(TAG, "decompression finished successfully");
       //   if(!dry_run && !disable_ota_reboot) {
@@ -164,13 +165,13 @@ esphome::ota::OTAResponseTypes CanopenOTAComponent::end(const char *expected_md5
   mz_deflateEnd(&this->stream);
 #else
   ret = esphome::ota::OTAResponseTypes::OTA_RESPONSE_OK;
-  if(!dry_run) {
+  if (!dry_run) {
     backend->set_update_md5(expected_md5);
   }
   // let's finish update asynchronously
   // as on stm32 platform calling backend->end() instantly reboots
   // and canopen block transfer is not finished
-  ota_finished_trigger->trigger();
+  this->ota_finish();
 #endif
   return ret;
 }
--- components/canopen/ota/ota.h
+++ components/canopen/ota/ota.h
@@ -14,16 +14,24 @@
 #include "esphome/core/preferences.h"
 #include "esphome/core/automation.h"
 
-#include "esphome/components/ota/ota_backend.h"
+#include "esphome/components/ota/ota_backend_factory.h"
 
 namespace esphome {
 namespace canopen {
+
+enum OtaState {
+  OTA_STATE_IDLE = 1,
+  OTA_STATE_WAIT1 = 2,
+  OTA_STATE_WAIT2 = 3,
+};
 
 class OtaFinishedTrigger : public Trigger<> {};
 
 class CanopenOTAComponent : public ota::OTAComponent {
   const char *TAG = "canopen_ota";
   OtaFinishedTrigger *ota_finished_trigger;
+  OtaState ota_state;
+  uint32_t start_time = 0;
 
 #ifdef OTA_COMPRESSION
   const static uint32_t BUF_SIZE = 8 * 1024;
@@ -36,9 +44,11 @@ class CanopenOTAComponent : public ota::OTAComponent {
 
   bool dry_run = false;
 
+  void ota_finish();
+
  public:
   bool disable_ota_reboot = false;
-  std::unique_ptr<esphome::ota::OTABackend> backend;
+  ota::OTABackendPtr backend;
   void setup() override;
   //   void dump_config() override;
   float get_setup_priority() const override;
